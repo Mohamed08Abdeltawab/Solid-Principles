f9710606b04b8cc25b781d81e16ed5821d8b7f38
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-08-31 21:11:32 +0300

    Module1-SRL -> Logging Service

---
+++ Code/Module1-(SRP)/02 - LoggingService/02 - LoggingService-SRP/02 - LoggingService-SRP/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Transactions;
+
+public class LoggingService_SRP
+{
+    public enum enLoggingType { ToFile, ToEventLog, ToDatabase }
+
+    public void Log(string message, enLoggingType LoggingType)
+    {
+
+        if (LoggingType == enLoggingType.ToFile)
+        {
+            LogToFile.log(message);
+        }
+        else if (LoggingType == enLoggingType.ToEventLog)
+        {
+            LogToEventLog.log(message);
+        }
+        else if (LoggingType == enLoggingType.ToDatabase)
+        {
+            LogToDatabse.log(message);
+        }
+    }
+
+    
+
+}
+
+static class LogToDatabse
+{
+    public static void log(string message)
+    {
+        Console.WriteLine($"\nLog to Database: {message}");
+    }
+}
+
+static class LogToEventLog
+{
+    public static void log(string message)
+    {
+        Console.WriteLine($"\nLog to Event Log: {message}");
+    }
+}
+
+static class LogToFile
+{
+    public static void log(string message)
+    {
+        Console.WriteLine($"\nLog to file: {message}");
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // Create an instance of the LoggingService
+        LoggingService_SRP LoggingService = new LoggingService_SRP();
+
+        // Log to File
+        LoggingService.Log("Error Occured line xxx.", LoggingService_SRP.enLoggingType.ToFile);
+
+        // Log to Event Log
+        LoggingService.Log("Error Occured line xxx.", LoggingService_SRP.enLoggingType.ToEventLog);
+
+        // Log to Database
+        LoggingService.Log("Error Occured line xxx.", LoggingService_SRP.enLoggingType.ToDatabase);
+
+        Console.ReadKey();
+
+    }
+}

7e826b74113644aacd74f4f4f19e858efd9b7469
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-09-12 23:15:48 +0300

    Moudle4-ISP Exapmles

---
+++ Code/Module4-(ISP)/Ex3 - Violating ISP - Payment/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+public interface IPayWithCreditCard
+{
+    public void PayWithCreditCard();
+}
+
+public interface IPayWithPayPal
+{
+    public void PayWithPayPal();
+}
+
+public interface IPayWithBitcoin
+{
+    public void PayWithBitcoin();
+}
+
+public class CreditCardPayment : IPayWithCreditCard
+{
+    public void PayWithCreditCard()
+    {
+        Console.WriteLine("Payment with credit card.");
+    }
+
+}
+
+public class PayPalPayment : IPayWithPayPal
+{
+
+    public void PayWithPayPal()
+    {
+        Console.WriteLine("Payment with PayPal.");
+    }
+}
+
+public class BitcoinPayment : IPayWithBitcoin
+{
+
+    public void PayWithBitcoin()
+    {
+        Console.WriteLine("Payment with Bitcoin.");
+    }
+}
+
+public class Payment : IPayWithCreditCard, IPayWithPayPal, IPayWithBitcoin
+{
+    public void PayWithCreditCard()
+    {
+        Console.WriteLine("Payment with credit card.");
+    }
+
+    public void PayWithPayPal()
+    {
+        Console.WriteLine("Payment with PayPal.");
+    }
+
+    public void PayWithBitcoin()
+    {
+        Console.WriteLine("Payment with Bitcoin.");
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        IPayWithCreditCard creditCardPayment = new CreditCardPayment();
+        creditCardPayment.PayWithCreditCard();
+        // creditCardPayment.PayWithPayPal();
+        // creditCardPayment.PayWithBitcoin ();
+
+        IPayWithPayPal payPalPayment = new PayPalPayment();
+        payPalPayment.PayWithPayPal();
+        // payPalPayment.PayWithBitcoin();
+
+        IPayWithBitcoin bitcoinPayment = new BitcoinPayment();
+        bitcoinPayment.PayWithBitcoin();
+
+        Payment payment = new Payment();
+        payment.PayWithCreditCard();
+
+        Console.ReadKey();
+
+    }
+}
---
+++ Code/Module4-(ISP)/Ex5 - Violating ISP - Device/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public interface IMakeCall
+{
+    void MakeCall();
+}
+
+public interface ITakePhoto
+{
+    void TakePhoto();
+}
+
+public interface ISendEmail
+{
+    void SendEmail();
+}
+
+public interface IUseGPS
+{
+    void UseGPS();
+}
+
+public class Smartphone : IMakeCall, ITakePhoto, ISendEmail, IUseGPS
+{
+    public void MakeCall()
+    {
+        Console.WriteLine("Making a call.");
+    }
+
+    public void TakePhoto()
+    {
+        Console.WriteLine("Taking Photo.");
+    }
+
+    public void SendEmail()
+    {
+        Console.WriteLine("Sending an email.");
+    }
+
+    public void UseGPS()
+    {
+        Console.WriteLine("Using GPS.");
+    }
+}
+
+public class Computer : ISendEmail
+{
+    public void SendEmail()
+    {
+        Console.WriteLine("Sending an email.");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Smartphone smartphone = new Smartphone();
+        Console.WriteLine("SmartPhone:");
+        smartphone.MakeCall();
+        smartphone.TakePhoto();
+        smartphone.SendEmail();
+        smartphone.UseGPS();
+
+        Console.WriteLine();
+
+        Computer computer = new Computer();
+        Console.WriteLine("Computer:");
+        computer.SendEmail();
+
+        Console.ReadKey();
+    }
+}
